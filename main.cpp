9e1918a3aadcbbeb9e4be491b4d37e7341c7e2c3
ashX04 <ashu0499@hotmail.com>
2025-01-19 16:54:21 +0530

    window opening using raylib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,36 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
-  std::cout << "Hello World!!!" << std::endl;
+  const int screenWidth = 800;
+  const int screenHeight = 800;
+  InitWindow(screenWidth, screenHeight, "Raylib Window");
+
+  Vector2 circlePosition = {400, 300};
+  float circleRadius = 50;
+  float moveSpeed = 0.5f;
+  while (!WindowShouldClose()) {
+    if (IsKeyDown(KEY_RIGHT)) {
+      circlePosition.x += moveSpeed; // Move right
+    }
+    if (IsKeyDown(KEY_LEFT)) {
+      circlePosition.x -= moveSpeed; // Move left
+    }
+    if (IsKeyDown(KEY_UP)) {
+      circlePosition.y -= moveSpeed; // Move up
+    }
+    if (IsKeyDown(KEY_DOWN)) {
+      circlePosition.y += moveSpeed; // Move down
+    }
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    DrawCircleV(circlePosition, circleRadius, BLUE);
+
+    DrawText("Hello World", 10, 10, 20, DARKGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+
   return 0;
 }
